9338c61efc41467da02fb817915739d836f59a23
Trevor <trevwilliams107@berkeley.edu>
2020-06-08 21:29:40 -0700

    can create and add to google sheets, does not display private playlists

--- albums.py
+++ albums.py
@@ -1,13 +1,148 @@
+from __future__ import print_function
+
+#spotify imports
 import sys
 import spotipy
 import spotipy.util as util
 from spotipy.oauth2 import SpotifyClientCredentials
 
-# util.prompt_for_user_token('trevwilliams107',
-#                            scope='playlist-modify-private,playlist-modify-public',
-#                            client_id='20624df21d7c41288ee2b990bcd14050',
-#                            client_secret='c3e9c3e71deb4d09ba2ef2441c305cc8',
-#                            redirect_uri='https://localhost:8000')
+#GOOGLE IMPORTS AND TOKEN STUFF
+
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from pprint import pprint
+from googleapiclient import discovery
+
+google_cid = '709831756208-hc395jqnaeq4gmrhgdp27243v38pbe2f.apps.googleusercontent.com'
+google_csec = '-W_7SzJWXQbGE1zzKhA5wsdm'
+
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+SPREADSHEET_ID = '1NbsNJd2C2bJMwI8mRA0UPQVq5PNhnh-qxDtJbfYFu3E'
+
+
+
+def createSpreadsheet(sheet_title):
+
+    # TODO: Change placeholder below to generate authentication credentials. See
+    # https://developers.google.com/sheets/quickstart/python#step_3_set_up_the_sample
+    #
+    # Authorize using one of the following scopes:
+    #     'https://www.googleapis.com/auth/drive'
+    #     'https://www.googleapis.com/auth/drive.file'
+    #     'https://www.googleapis.com/auth/spreadsheets'
+
+    creds = None
+        # The file token.pickle stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first
+        # time.
+
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = discovery.build('sheets', 'v4', credentials=creds)
+
+
+
+    # The spreadsheet to apply the updates to.
+    spreadsheet_id = SPREADSHEET_ID  # TODO: Update placeholder value.
+
+    batch_update_spreadsheet_request_body = {
+        # A list of updates to apply to the spreadsheet.
+        # Requests will be applied in the order they are specified.
+        # If any request is not valid, no requests will be applied.
+        "requests": [
+        {
+          "addSheet": {
+            "properties": {
+              "title": sheet_title,
+              "gridProperties": {
+                "rowCount": 500,
+                "columnCount": 12
+              },
+              "tabColor": {
+                "red": 1.0,
+                "green": 0.3,
+                "blue": 0.4
+              }
+            }
+          }
+        }
+      ]  # TODO: Update placeholder value.
+
+        # TODO: Add desired entries to the request body.
+    }
+
+    request = service.spreadsheets().batchUpdate(spreadsheetId=spreadsheet_id, body=batch_update_spreadsheet_request_body)
+    response = request.execute()
+
+def appendSong():
+
+    creds = None
+        # The file token.pickle stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first
+        # time.
+
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = discovery.build('sheets', 'v4', credentials=creds)
+
+    spreadsheet_id = SPREADSHEET_ID  # TODO: Update placeholder value.
+
+    # The A1 notation of a range to search for a logical table of data.
+    # Values will be appended after the last row of the table.
+    range_ = 'Sheet1!A1:B1'  # TODO: Update placeholder value.
+
+    # How the input data should be interpreted.
+    value_input_option = 'RAW'  # TODO: Update placeholder value.
+
+    # How the input data should be inserted.
+    insert_data_option = 'INSERT_ROWS'  # TODO: Update placeholder value.
+
+    value_range_body = {
+        # TODO: Add desired entries to the request body.
+        'values' :  [[3,4]]
+        }
+
+    request = service.spreadsheets().values().append(spreadsheetId=spreadsheet_id, range=range_, valueInputOption=value_input_option, insertDataOption=insert_data_option, body=value_range_body)
+    response = request.execute()
+
+
+
+
+
+util.prompt_for_user_token('trevwilliams107',
+                           scope='playlist-modify-private,playlist-modify-public',
+                           client_id='20624df21d7c41288ee2b990bcd14050',
+                           client_secret='c3e9c3e71deb4d09ba2ef2441c305cc8',
+                           redirect_uri='https://localhost:8000')
 
 def show_tracks(tracks):
     for i, item in enumerate(tracks['items']):
@@ -24,7 +159,11 @@ if __name__ == '__main__':
         print("usage: python user_playlists.py [username]")
         sys.exit()
 
-    token = util.prompt_for_user_token(username)
+    token = util.prompt_for_user_token(username,
+                               scope='playlist-modify-private,playlist-modify-public',
+                               client_id='20624df21d7c41288ee2b990bcd14050',
+                               client_secret='c3e9c3e71deb4d09ba2ef2441c305cc8',
+                               redirect_uri='https://localhost:8000')
 
     if token:
         sp = spotipy.Spotify(auth=token)
